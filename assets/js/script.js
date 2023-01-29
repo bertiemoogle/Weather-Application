19a8f61f5409015f3367fc2bbb9ac665b89b3146
bertiemoogle <bertiemoogle@gmail.com>
2023-01-29 16:14:21 +0000

    Accessing two different APIs - one to get the co-ordinates of the city, and one to use the co-ordinates to get the weather forecast.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,31 @@
-"use strict";
+const apiKey = "032156b075050ee7d43e1bf4ebcd4df6";
+
+let latitude;
+let longitude;
+let city = "manchester";
+let response;
+let queryURL1 = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appID=" + apiKey;
+
+
+
+fetch(queryURL1)
+.then(response => response.json())
+.then(function(response) {
+    latitude = response[0].lat;
+    longitude = response[0].lon;
+
+    let queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+
+    fetch(queryURL2)
+    .then(response => response.json())
+    .then(function(response) {
+        console.log(queryURL2);
+        console.log(response);
+    });
+    
+    
+});
+
+
+//   celciusTemp = Math.floor(temp - 273.15);
 
