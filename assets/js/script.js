0b90b28105dc4516f30a0553b0fa7aac282f15ce
bertiemoogle <bertiemoogle@gmail.com>
2023-01-29 19:57:33 +0000

    Current date working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ const apiKey = "032156b075050ee7d43e1bf4ebcd4df6";
 
 let latitude;
 let longitude;
+let weatherIcon;
+let currentDate = new Date().toJSON().slice(0, 10);
 let city = "berlin";
 let response;
 let queryURL1 = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appID=" + apiKey;
@@ -22,6 +24,10 @@ fetch(queryURL1)
         console.log(queryURL2);
         console.log(response);
 
+        if (response.list[0].weather[0].id === 804) {
+            weatherIcon = "http://openweathermap.org/img/wn/04n@80x.png";
+        };
+
         let temp = response.list[0].main.temp;
         let celciusTemp = Math.floor(temp - 273.15)
 
@@ -29,16 +35,22 @@ fetch(queryURL1)
 
         let humidity = response.list[0].main.humidity;
 
+        document.querySelector("#date").innerHTML = currentDate;
         document.querySelector("#temp").innerHTML = "Temp: " + celciusTemp + "℃";
         document.querySelector("#wind").innerHTML = "Wind: " + wind + "mph";
         document.querySelector("#humid").innerHTML = "Humidity: " + humidity;
+        document.querySelector("#card-one-temp").innerHTML = "Temp: " + celciusTemp + "℃";
+        document.querySelector("#card-one-wind").innerHTML = "Wind: " + wind + "mph";
+        document.querySelector("#card-one-humid").innerHTML = "Humidity: " + humidity;
+        document.querySelector(".card-title").innerHTML = currentDate;
 
     });
 
 
 });
 
-
+// let currentDate = new Date().toJSON().slice(0, 10);
+// console.log(currentDate);
 
 
 //   celciusTemp = Math.floor(temp - 273.15);
